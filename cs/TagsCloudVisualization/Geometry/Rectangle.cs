c20de37058d6e6e73c31d271d2d6a4c759b6a2db
Vladimir Golubev <gor.prast@bk.ru>
2025-11-27 03:03:16 +0500

    feat: add rectnagles intersection check method

--- cs/TagsCloudVisualization/Geometry/Rectangle.cs
+++ cs/TagsCloudVisualization/Geometry/Rectangle.cs
@@ -4,10 +4,21 @@ public class Rectangle
 {
     public Point Center { get; }
     public Size Size { get; }
+    
+    public double Left => Center.X - Size.Width / 2;
+    public double Right => Center.X + Size.Width / 2;
+    public double Top => Center.Y - Size.Height / 2;
+    public double Bottom => Center.Y + Size.Height / 2;
 
     public Rectangle(Point center, Size size)
     {
         Center = center;
         Size = size;
+    }
+    
+    public bool IntersectsWith(Rectangle other)
+    {
+        return Left < other.Right && Right > other.Left &&
+               Top < other.Bottom && Bottom > other.Top;
     }
 }
