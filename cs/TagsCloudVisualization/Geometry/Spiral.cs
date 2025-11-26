fd9bb946221edb362ebbdbf615ae89d0f75ea281
Vladimir Golubev <gor.prast@bk.ru>
2025-11-27 03:18:21 +0500

    feat: add spiral class

---
+++ cs/TagsCloudVisualization/Geometry/Spiral.cs
@@ -0,0 +1,27 @@
+namespace TagsCloudVisualization.Geometry;
+
+public class Spiral
+{
+    private readonly Point center;
+    private double currentAngle;
+    private readonly double angleStep;
+    private readonly double distancePerRevolution;
+
+    public Spiral(Point center, double angleStep = 0.1, double distancePerRevolution = 1.0)
+    {
+        this.center = center;
+        this.angleStep = angleStep;
+        this.distancePerRevolution = distancePerRevolution;
+    }
+
+    public Point GetNextPoint()
+    {
+        var radius = distancePerRevolution / (2 * Math.PI) * currentAngle;
+        var x = center.X + radius * Math.Cos(currentAngle);
+        var y = center.Y + radius * Math.Sin(currentAngle);
+        
+        currentAngle += angleStep;
+        
+        return new Point(x, y);
+    }
+}
