be5bbc132591e8aeb255d1f698e47de3f2cdc70d
Vladimir Golubev <gor.prast@bk.ru>
2025-11-27 03:09:11 +0500

    test: add test to check intersections of rectangles

--- cs/Tests/CircularCloudLayouterTests.cs
+++ cs/Tests/CircularCloudLayouterTests.cs
@@ -26,4 +26,33 @@ public class CircularCloudLayouterTests
         rectangle.Center.Should().Be(center);
     }
     
+    [Test]
+    public void PutNextRectangle_ManyRectangles_ShouldNotIntersects()
+    {
+        foreach (var size in GenerateSizes(10, 20, 30))
+        {
+            layouter.PutNextRectangle(size);
+        }
+        
+        var rectangles = layouter.Rectangles.ToList();
+
+        foreach (var r1 in rectangles)
+        {
+            foreach (var r2 in rectangles.Where(r2 => r2 != r1))
+            {
+                r1.IntersectsWith(r2).Should().BeFalse();
+            }
+        }
+    }
+    
+    private IEnumerable<Size> GenerateSizes(int count, int minSize, int maxSize)
+    {
+        var random = new Random();
+        for (int i = 0; i < count; i++)
+        {
+            var width = random.Next(minSize, maxSize);
+            var height = random.Next(minSize, maxSize);
+            yield return new Size(width, height);
+        }
+    }
 }
