ab0a7cc5feeb3807169856589a9fb72fbf6ce98f
Vladimir Golubev <gor.prast@bk.ru>
2025-11-27 06:56:58 +0500

    feat: add setting for cloud visualizer

---
+++ cs/TagsCloudVisualization/Visualizers/VisualizerSettings.cs
@@ -0,0 +1,45 @@
+using System.Drawing;
+
+namespace TagsCloudVisualization.Visualizers;
+
+public class VisualizerSettings
+{
+    public int Width { get; }
+    public int Height { get; }
+    public double CloudScaleFactor { get; }
+    public Color BackgroundColor { get; }
+    public Color RectangleColor { get; }
+    public Color RectangleBorderColor { get; }
+    public float RectangleBorderWidth { get; }
+
+    public VisualizerSettings(
+        int width,
+        int height,
+        double cloudScaleFactor,
+        Color backgroundColor,
+        Color rectangleColor,
+        Color rectangleBorderColor, 
+        float rectangleBorderWidth)
+    {
+        Width = width;
+        Height = height;
+        CloudScaleFactor = cloudScaleFactor;
+        BackgroundColor = backgroundColor;
+        RectangleColor = rectangleColor;
+        RectangleBorderColor = rectangleBorderColor;
+        RectangleBorderWidth = rectangleBorderWidth;
+    }
+
+    public static VisualizerSettings Default()
+    {
+        return new VisualizerSettings(
+            800,
+            600,
+            0.8,
+            Color.White,
+            Color.Plum,
+            Color.Black,
+            1f
+        );
+    }
+}
