148b0e1b104eb87a80ac8f3cfb2b440119f4ac1a
Vladimir Golubev <gor.prast@bk.ru>
2025-12-02 01:32:31 +0500

    fix: fix GetVertices and GetArea method

--- cs/TagsCloudVisualization/Geometry/Extensions/RectangleExtensions.cs
+++ cs/TagsCloudVisualization/Geometry/Extensions/RectangleExtensions.cs
@@ -4,28 +4,17 @@ namespace TagsCloudVisualization.Geometry.Extensions;
 
 public static class RectangleExtensions
 {
-    public static bool IntersectsWith(this Rectangle rectangle, Rectangle other)
+    public static IEnumerable<Point> GetVertices(this Rectangle rectangle)
     {
-        return rectangle.Left < other.Right
-               && rectangle.Right > other.Left
-               && rectangle.Top < other.Bottom
-               && rectangle.Bottom > other.Top;
-    }
-
-    public static Point[] GetVertices(this Rectangle rectangle)
-    {
-        return
-        [
-            new Point(rectangle.Left, rectangle.Top),    
-            new Point(rectangle.Left, rectangle.Bottom), 
-            new Point(rectangle.Right, rectangle.Top),  
-            new Point(rectangle.Right, rectangle.Bottom)
-        ];
+        yield return new Point(rectangle.Left, rectangle.Top);
+        yield return new Point(rectangle.Left, rectangle.Bottom);
+        yield return new Point(rectangle.Right, rectangle.Top);
+        yield return new Point(rectangle.Right, rectangle.Bottom);
     }
 
     public static double GetArea(this Rectangle rectangle)
     {
-        return rectangle.Size.Height * rectangle.Size.Width;
+        return rectangle.Height * rectangle.Width;
     }
 
     public static Rectangle MoveInDirection(this Rectangle rectangle, Point direction, double distance)
